2d39e3d4703098b8ee6f139c9c0845ca03fb9b78
zweatshirt <>
2021-01-23 13:17:05 -0600

    Adding

---
+++ main.py
@@ -0,0 +1,11 @@
+# Imports for the project #
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# Note: We can separate the different parts into different files
+# then just add everything here.
+
+## Main ##
+
+
+def main():
